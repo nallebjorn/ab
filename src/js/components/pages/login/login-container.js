01709c16011c472b18912e0afe07e2063c0c443d
nallebjorn <vladpanda1234@gmail.com>
2019-10-31 19:13:31 +0300

    login page

--- src/js/components/pages/login/login-container.js
+++ src/js/components/pages/login/login-container.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Form from "./login-form";
+import {setHeaderTitle} from 'Actions/actions'
+
 
 const LoginPage = () => {
-    const onSubmitHandler =  values => {
-        console.log(values);
+    const dispatch = useDispatch();
+    const onSubmitHandler =  ({username, password}) => {
+        dispatch(setHeaderTitle(`Welcome to site, ${username}.`))
     };
 
     return (
